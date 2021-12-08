c3f60aa8e5bbc20d3c4fc0684ce08dba5d62d44d
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-12-08 08:47:13 -0300

    initial commit

---
+++ src/test/java/artursfg/beerservice/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package artursfg.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
