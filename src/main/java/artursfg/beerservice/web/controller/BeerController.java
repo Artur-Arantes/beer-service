0940cbf9b40d5065141eea95c357799a8141cb52
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-12-08 14:01:58 -0300

    adding controller tests.

---
+++ src/main/java/artursfg/beerservice/web/controller/BeerController.java
@@ -0,0 +1,34 @@
+package artursfg.beerservice.web.controller;
+
+
+import artursfg.beerservice.web.model.BeerDto;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+  @RequestMapping("/{beerId}")
+  public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID Id) {
+    return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+    return new ResponseEntity(HttpStatus.CREATED);
+  }
+
+  @PutMapping("/{beerId}")
+  public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId,
+                                       @RequestBody BeerDto beerDto) {
+    return new ResponseEntity(HttpStatus.NO_CONTENT);
+  }
+}
