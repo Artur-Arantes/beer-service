d3cea650361fa841b1db2faedef45e8043576fd3
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-12-10 10:55:08 -0300

    added beer dto and lombock to the pom.xml.

--- src/main/java/artursfg/beerservice/web/model/BeerDto.java
+++ src/main/java/artursfg/beerservice/web/model/BeerDto.java
@@ -3,30 +3,37 @@ package artursfg.beerservice.web.model;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class BeerDto {
-
+  @NonNull
   private UUID id;
+
   private Integer version;
 
   private OffsetDateTime createdDate;
   private OffsetDateTime lastModifiedDate;
 
+  @NotBlank
   private String beerName;
 
   private BeerStyleEnum beerStyle;
 
+  @NonNull
   private Long upc;
 
+  @NonNull
   private BigDecimal price;
+
 
   private Integer quantityOnHand;
 }
