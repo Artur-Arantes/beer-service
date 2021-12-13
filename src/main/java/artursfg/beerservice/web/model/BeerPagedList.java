08b4a8681249e1231e9c6836536f80d5386f344b
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-12-13 13:45:29 -0300

    adding json creator.

--- src/main/java/artursfg/beerservice/web/model/BeerPagedList.java
+++ src/main/java/artursfg/beerservice/web/model/BeerPagedList.java
@@ -1,7 +1,10 @@
 package artursfg.beerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import java.util.List;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 public class BeerPagedList extends PageImpl<BeerDto> {
@@ -14,5 +17,17 @@ public class BeerPagedList extends PageImpl<BeerDto> {
 
   public BeerPagedList(List<BeerDto> content) {
     super(content);
+  }
+  public BeerPagedList(@JsonProperty("content") List<BeerDto> content,
+                       @JsonProperty("number") int number,
+                       @JsonProperty("size") int size,
+                       @JsonProperty("totalElements") Long totalElements,
+                       @JsonProperty("pageable")JsonNode pageable,
+                       @JsonProperty("last") boolean last,
+                       @JsonProperty("totalPages") int totalPages,
+                       @JsonProperty("sort") JsonNode sort,
+                       @JsonProperty("first") boolean first,
+                       @JsonProperty("numberOfElements") int numberOfElements){
+    super(content, PageRequest.of(number,size), totalElements);
   }
 }
